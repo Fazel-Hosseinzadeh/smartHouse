f7f5bd46554d896d7ef27b5de0c5b4139e83fc82
Fazel HosseinzadehDastak <fazelhoseinzadeh@yahoo.com>
2023-05-10 18:56:50 +0000

    add listener for inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 /**
  * Waiting all elements are loaded and the adding
  * event listenrs to switchs and sliders
@@ -6,11 +7,60 @@ document.addEventListener('DOMContentLoaded' , function(){
     let switchs = document.getElementsByTagName('input');
     for (let swt of switchs){ 
         swt.addEventListener('click', function(){
-            if(this.getelementBy )
+          let swType= this.getAttribute('data-type');
+          switch (swType) {
+            case living-room-light1:
+                
+                break;
+                case whole-house-lights:
+                
+                break;
+                case whole-house-desired-temp:
+                
+                break;
+                case whole-house-fans:
+                
+                break;
+                case kitchen-light:
+                
+                break;
+                case kitchen-desired-temp:
+                
+                break;
+                case living-room-light1:
+                
+                break;
+                case living-room-light2:
+                
+                break;
+                case living-room-desired-temp:
+                
+                break;
+                case living-room-fan:
+                
+                break;
+                case living-room-fan-speed:
+                
+                break;
+
+                case bed-room-light:
+                
+                break;
+
+                case bed-room-desired-temp:
+                
+                break;
+
+                case garage-light:
+                
+                break;
+
+            default:
+                break;
+          }
         })
     }
     })
-})
 
 
 function toggleSwitch(){
