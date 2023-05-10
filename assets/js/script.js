6b94ef551d9fffda25f838ab07f585cf0ce7851d
Fazel HosseinzadehDastak <fazelhoseinzadeh@yahoo.com>
2023-05-10 18:17:39 +0000

    replace id with data type attribute for inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+/**
+ * Waiting all elements are loaded and the adding
+ * event listenrs to switchs and sliders
+ */
+document.addEventListener('DOMContentLoaded' , function(){
+    let switchs = document.getElementsByTagName('input');
+    for (let swt of switchs){ 
+        swt.addEventListener('click', function(){
+            if(this.getelementBy )
+        })
+    }
+    })
+})
+
+
 function toggleSwitch(){
 
 }
