aff28400dec5255e4537859e70a22c314432d042
Fazel HosseinzadehDastak <fazelhoseinzadeh@yahoo.com>
2023-05-11 05:55:51 +0000

    JS add initiat status fro each place

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,53 +4,74 @@
  * event listenrs to switchs and sliders
  */
 document.addEventListener('DOMContentLoaded' , function(){
+
+    /*Initiating the whole house statuse*/
+    let wholeHouse=[
+        {lights: false,
+        image: 'gray'} , 
+        {curentTEmp: 15,
+        sliderTemp: 15},
+        {fan: false ,
+        fanSpeed: 1}
+        ];
+
+    /*Initiating the kitchen statuse*/
+    let kitchen =[
+        {light: false,
+        image: 'gray'} , 
+        {curentTEmp: 15,
+        sliderTemp: 15}
+        ];
+    /*Initiating the livingRoom statuse*/
+    let livingRoom=[
+        {light1: false,
+        image: 'gray'} , 
+        {light2: false,
+        image: 'gray'} ,
+        {curentTEmp: 15,
+        sliderTemp: 15} ,
+        {fan: false,
+        sliderSpeed: 1}
+        ];
+    /*Initiating the bedRoom statuse*/
+    let bedRoom=[
+        {light: false,
+        image: 'gray'} , 
+        {curentTEmp: 15,
+        sliderTemp:15}
+        ];
+    /*Initiating the garage statuse*/
+    let garage=[
+        {light: false,
+        image: 'gray'} 
+        ];
+        console.log(garage);
+
+
     let switchs = document.getElementsByTagName('input');
     for (let swt of switchs){ 
         swt.addEventListener('click', function(){
             let swType= this.getAttribute('data-type');
           switch (swType) {
+            /*in case a switch is clicked */
                 case 'whole-house-lights':
-                    toggleSwitch(swType);
-                break;
-                case 'whole-house-desired-temp':
-                    alert("hi");
-                    sliderReader(swType);
-                break;
                 case 'whole-house-fans':
-                    toggleSwitch(swType);
-                break;
                 case 'kitchen-light':
-                    toggleSwitch(swType);
-                break;
-                case 'kitchen-desired-temp':
-                    sliderReader(swType);
-                break;
                 case 'living-room-light1':
-                    toggleSwitch(swType);
-                break;
                 case 'living-room-light2':
-                    toggleSwitch(swType);
-                break;
-                case 'living-room-desired-temp':
-                    sliderReader(swType);
-                break;
                 case 'living-room-fan':
-                    toggleSwitch(swType);
-                break;
-                case 'living-room-fan-speed':
-                    sliderReader(swType);
-                break;
-
                 case 'bed-room-light':
-                    toggleSwitch(swType);
+                case 'garage-light':
+                    
+                    toggleSwitch(swType,this.value);
                 break;
-                
+                /*in case a slider is clicked */
+                case 'whole-house-desired-temp':
+                case 'kitchen-desired-temp':
+                case 'living-room-desired-temp':
+                case 'living-room-fan-speed':
                 case 'bed-room-desired-temp':
                     sliderReader(swType);
-                break;
-
-                case 'garage-light':
-                    toggleSwitch(swType);
                 break;
 
             default:
@@ -62,12 +83,14 @@ document.addEventListener('DOMContentLoaded' , function(){
     })
 
 
-function toggleSwitch(dataType){
+function toggleSwitch(dataType, val){
+
     
     }
 
 
 function sliderReader(dataType){
+    
     
 }
 
